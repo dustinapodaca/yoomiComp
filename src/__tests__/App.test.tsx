26f4a1dba94c241795e55a220c58777c8a07a0ae
Dustin Apodaca <dustinapodaca@gmail.com>
2023-11-06 12:24:27 -0700

    working functionality with a few tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+// Tests for App.tsx
+
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import { metadata } from "../mockData/metadata";
 
-test('renders learn react link', () => {
+test('Renders Setup', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Setup/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("Renders Movement", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Movement/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("Renders Tips", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Tips/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("Renders Exercise Name", () => {
+  render(<App />);
+  const firstExerciseName = screen.getByText(metadata[0].friendlyExerciseName);
+  expect(firstExerciseName).toBeInTheDocument();
+});
+
